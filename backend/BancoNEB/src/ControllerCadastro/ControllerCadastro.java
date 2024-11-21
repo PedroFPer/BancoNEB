933a1ea6b0ffe09bf60e469b42fab4a29c233aec
Pedro <pedro.f.pereira@ba.estudante.senai.br>
2024-11-21 12:55:38 -0300

    Finalmente!

--- backend/BancoNEB/src/ControllerCadastro/ControllerCadastro.java
+++ backend/BancoNEB/src/ControllerCadastro/ControllerCadastro.java
@@ -20,10 +20,126 @@ public class ControllerCadastro {
     UtilVericCpf utilVericCpf = new UtilVericCpf();
     UtilVericSenha utilVericSenha = new UtilVericSenha();
 
-    public boolean controllerCadastroClie(ClienteDTO clienteDTO) {
-       Scanner scanner = new Scanner(System.in);
-       boolean vericCadas = serviceCadastro.serviceCadastroCliente(clienteDTO);
-       return vericCadas;
+    public void controllerCadastroClie() {
+        Scanner scanner = new Scanner(System.in);
+        LocalDate dataNascimento;
+        String cpf;
+        String telefone;
+        String senhaEntrada;
+        String senhaAutorizacao;
+
+        System.out.println("Informe o nome do cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        do {
+            try {
+                System.out.println("Informe a data de nascimento (yyyy-MM-dd):");
+                String dataNasc = scanner.nextLine();
+
+                dataNascimento = LocalDate.parse(dataNasc, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+                LocalDate hoje = LocalDate.now();
+
+                Period periodo = Period.between(dataNascimento, hoje);
+
+                if (periodo.getYears() > 16) {
+                    break;
+                } else {
+                    System.out.println("É necessario ter pelo menos 16 anos para poder criar uma conta no banco");
+                }
+
+            } catch (DateTimeParseException e) {
+                System.out.println("Formato errado, por favor tente novamente");
+            }
+        } while (true);
+
+        do {
+            System.out.println("Informe o CPF (11 dígitos):");
+            cpf = scanner.nextLine();
+
+            boolean vericCpf = utilVericCpf.vericQuant(cpf);
+
+            if (vericCpf) {
+                boolean controlVericCpfExis = serviceRelatClien.serviceVericCpfExis(cpf);
+
+                if (controlVericCpfExis) {
+                    System.out.println("CPF já existe no sistema!");
+
+                } else {
+                    break;
+                }
+               
+            } else {
+                System.out.println("Formato errado! O Cpf deve conter 11 caracteres");
+            }
+
+        } while (true);
+
+        do {
+            System.out.println("Informe o telefone (13 dígitos):");
+            telefone = scanner.nextLine();
+
+            boolean vericTelefone = utilVerifTelefone.vericQuant(telefone);
+
+            if (vericTelefone) {
+                break;
+            } else {
+                System.out.println("Formato errado! O telefone deve conter 13 caracteres, junto do DDD");
+            }
+        } while (true);
+
+        System.out.println("Informe o email:");
+        String email = scanner.nextLine();
+
+        do {
+            System.out.println("Informe a senha de entrada (8 caracteres):");
+            senhaEntrada = scanner.nextLine();
+
+            boolean vericSenhaEntr = utilVericSenha.vericQuant(senhaEntrada);
+
+            if (vericSenhaEntr) {
+                break;
+            } else {
+                System.out.println("Formato errado! A senha de entrada deve conter 8 caracteres");
+            }
+
+        } while (true);
+
+        do {
+            System.out.println("Informe a senha de autorização (8 caracteres):");
+            senhaAutorizacao = scanner.nextLine();
+
+            boolean vericSenhaEntr = utilVericSenha.vericQuant(senhaAutorizacao);
+
+            if (vericSenhaEntr) {
+                break;
+            } else {
+                System.out.println("Formato errado! A senha de entrada deve conter 8 caracteres");
+            }
+
+        } while (true);
+
+        System.out.println("Informe o logradouro:");
+        String logradouro = scanner.nextLine();
+
+        System.out.println("Informe o bairro:");
+        String bairro = scanner.nextLine();
+
+        System.out.println("Informe a cidade:");
+        String cidade = scanner.nextLine();
+
+        System.out.println("Informe o estado:");
+        String estado = scanner.nextLine();
+
+        System.out.println("Informe o país:");
+        String pais = scanner.nextLine();
+
+        System.out.println("Informe a referência:");
+        String referencia = scanner.nextLine();
+
+        ClienteDTO clienteDTO = new ClienteDTO(nomeCliente, cpf, telefone, logradouro, bairro, cidade, estado, pais, email, senhaEntrada, referencia, dataNascimento, senhaAutorizacao);
+
+        serviceCadastro.serviceCadastroCliente(clienteDTO);
 
     }
 
--- backend/BancoNEB/src/ControllerLogin/ControllerLogin.java
+++ backend/BancoNEB/src/ControllerLogin/ControllerLogin.java
@@ -9,13 +9,56 @@ import java.util.Scanner;
 public class ControllerLogin {
 
     Scanner scanner = new Scanner(System.in);
-    
-    ServiceVericCreden serviceVericCreden = new ServiceVericCreden();
 
-    public Integer loginEntrada(String cpf, String senhaEntra) {
+    UtilVericCpf vericCpf = new UtilVericCpf();
+    UtilVericSenha vericSenha = new UtilVericSenha();
 
-        Integer idClienteControl = serviceVericCreden.serviceResqLoginCli(cpf, senhaEntra);
-        return idClienteControl;
+    public Integer loginEntrada() {
+        boolean vericQuantCpf;
+        boolean vericQuantSenha;
+        String cpf;
+        String senha;
 
+        do {
+            do {
+                System.out.println("Digite o seu cpf:");
+                cpf = scanner.nextLine();
+
+                vericQuantCpf = vericCpf.vericQuant(cpf);
+
+                if (vericQuantCpf) {
+                    break;
+                } else {
+                    System.out.println("CPF invalido! CPF precisa de 11 digitos");
+                }
+
+            } while (true);
+
+            do {
+                System.out.println("Digite sua senha:");
+                senha = scanner.nextLine();
+
+                vericQuantSenha = vericSenha.vericQuant(senha);
+
+                if (vericQuantSenha) {
+                    break;
+                } else {
+                    System.out.println("Senha invalida! A senha precisa de 8 digitos");
+                }
+
+            } while (true);
+
+            ServiceVericCreden serviceVericCreden = new ServiceVericCreden();
+
+            Integer idClienteControl = serviceVericCreden.serviceResqLoginCli(cpf, senha);
+
+            if (idClienteControl != null) {
+                return idClienteControl;
+            } else {
+                return null;
+            }
+
+        } while (true);
     }
+
 }
